c5d0283d8fe2c13c80ecc5840f0cc70e04c6c574
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-15 03:11:24 +0000

    reques

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -11,11 +11,11 @@ class NeuralNetwork():
         " initialized variables "
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        if nx <= 1:
             raise ValueError("nx must be a positive integer")
         if type(nodes) is not int:
             raise TypeError("nodes must be an integer")
-        if nodes < 1:
+        if nodes <= 1:
             raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.normal(size=(nodes, nx))
