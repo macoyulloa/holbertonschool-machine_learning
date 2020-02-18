57e0ce35e9e2d7ac749c57aa6fd9b8e11544f5b8
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-18 19:02:44 +0000

    declare deep learning

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """deepNeuronalNetwork performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """ defining, initialized the variables """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.nx = nx
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for l in range(self.L):
+            if layers[l] <= 0 and type(layers[l]) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            # initialized weight using He et al, layer[0]=nodes of first layer
+            if l is 0:
+                he_init = np.random.randn(layers[l], nx)*np.sqrt(2/nx)
+                self.weights['W' + str(l+1)] = he_init
+            if l > 0:
+                he_init1 = np.random.randn(layers[l], layers[l-1])
+                he_init2 = np.sqrt(2/layers[l-1])
+                self.weights['W' + str(l+1)] = he_init1 * he_init2
+            # bias network initialized to 0's
+            self.weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
