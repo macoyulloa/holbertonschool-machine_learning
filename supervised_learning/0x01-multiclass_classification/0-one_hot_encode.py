4e5a6d0956e7403eb81f2d8ee04d43b2c1c6365e
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-22 15:25:18 +0000

    special cases

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,6 +6,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """converts a numeric label into a vector"""
-    hot_encode = np.zeros((Y.size, Y.max() + 1))
+    if len(Y) == 0 or len(Y) != classes:
+        return None
+    hot_encode = np.zeros((classes, Y.shape[0]))
     hot_encode[Y, np.arange(Y.size)] = 1
     return hot_encode
