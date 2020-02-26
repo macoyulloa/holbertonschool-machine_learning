18d75dfcf5d7b63f62b1c6acc9bee03d23961713
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2020-02-26 04:12:28 +0000

    points

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -4,7 +4,34 @@
 import tensorflow as tf
 
 
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """saving the neural network"""
+    graph = tf.graph()
+    with graph.as_default():
+        x, y = create_placeholders(X_train.shape[0], Y_train.shape[0])
+        y_pred = forward_prop(x, layer_sizes, activations)
+        accuracy = calculate_accuracy(y, y_pred)
+        loss = calculate_loss(y, y_pred)
+        train_op = create_train_op(loss, alpha)
+        tf.add_to_collecton("my_colection", x, y, y_pred, accuracy, loss)
+    saver = tf.train.Saver()
+    with tf.Session(graph=graph) as session:
+        for i in range(iterations):
+            session.run(accuracy)
+            session.run(loss)
+            session.run(train_op)
 
+            counter += 1
+            if (i == 0) or (counter == 100) or (i == iterations-1):
+                print("After {} iterations").format(i)
+                print("Training Cost: {}".format(loss))
+                counter = 0
+        save_path = saver.save(session, save_path)
--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -4,6 +4,17 @@
 import tensorflow as tf
 
 
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+train = __import__('6-train').train
+
+
 def evaluate(X, Y, save_path):
     """evaluated a saved neural network"""
-
+    saver = tf.train.Saver()
+    with tf.Session() as session:
+        saver.restore(session, save_path)
+        session.run([accuracy, loss])
+        print("Test Accuracy: ", accuracy)
+        print("Test Cost: ", loss)
