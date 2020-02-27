f17ab450e1869e24bd8df3b90368323c4d22c47d
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-02-27 21:02:51 +0000

    files

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -33,15 +33,21 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     with tf.Session() as sess:
         sess.run(init)
         for i in range(iterations + 1):
-            sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-            sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+            cost_train = sess.run(loss,
+                                  feed_dict={x: X_train, y: Y_train})
+            accuracy_train = sess.run(accuracy,
+                                      feed_dict={x: X_train, y: Y_train})
+            cost_valid = sess.run(loss,
+                                  feed_dict={x: X_valid, y: Y_valid})
+            accuracy_valid = sess.run(accuracy,
+                                      feed_dict={x: X_valid, y: Y_valid})
 
-            if (i == 0) or (counter == 100) or (i == iterations-1):
-                print("After {} iterations").format(i)
-                print("Training Cost: {}".format(loss))
-                counter = 0
+            if (i % 100 == 0) or (i == iterations):
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(cost_train))
+                print("\tTraining Accuracy: {}".format(accuracy_train))
+                print("\tValidation Cost: {}".format(cost_valid))
+                print("\tValidation Accuracy: {}".format(accuracy_valid))
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         save_path = saver.save(sess, save_path)
