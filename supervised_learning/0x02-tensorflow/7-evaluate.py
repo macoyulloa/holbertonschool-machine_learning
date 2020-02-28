ab6d924dc9269c0dfe9d96130abbab0d93330811
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-02-28 19:57:29 +0000

    evaluate

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -8,13 +8,25 @@ calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 train = __import__('6-train').train
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 
 def evaluate(X, Y, save_path):
     """evaluated a saved neural network"""
-    saver = tf.train.Saver()
+
     with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
-        session.run([accuracy, loss])
-        print("Test Accuracy: ", accuracy)
-        print("Test Cost: ", loss)
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        y_predic = sess.run(y_pred, feed_dict={x: X, y: Y})
+        cost = sess.run(loss, feed_dict={x: X, y: Y})
+        accu = sess.run(accuracy, feed_dict={x: X, y: Y})
+
+        return y_predic, accu, cost
