2ffec408b75341f081497056274e64c05d774abd
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-02 15:53:17 +0000

    standarization

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Optimization tasks"""
+
+import numpy as np
+import tensorflow as tf
+
+
+def normalization_constants(X):
+    """calculates the normalization"""
+    mean = np.mean(X, axis=0)
+    standard_desv = np.std(X, axis=0)
+    return (mean, standard_desv)
