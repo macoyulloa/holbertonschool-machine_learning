57a9d9b17d0f3736d8ddf516e03f5f1726ec32de
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-02 22:32:34 +0000

    RMSPropagation

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -7,5 +7,5 @@ import numpy as np
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """gradient descent RMSProp optimization algorithm"""
     Sd = (beta2 * s) + ((1 - beta2) * grad**2)
-    W = var - (alpha * (grad / np.sqrt(Sd + epsilon)))
-    return (W, Sd)
+    v_updated = var - alpha * (grad / (Sd**(1/2) + epsilon))
+    return (v_updated, Sd)
