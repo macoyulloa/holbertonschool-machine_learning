faa7187a95e31401029c749c94ddd8fb6a151a45
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-03 17:35:22 +0000

    correction

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -6,5 +6,5 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """learming rate decay"""
-    alpha = (alpha / (1 + (decay_rate * int(global_step / decay_step))))
+    alpha = (alpha / (1 + (decay_rate * int(global_step/decay_step))))
     return alpha
