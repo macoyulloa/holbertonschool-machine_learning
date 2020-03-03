bf49986916256a8cb2793c0136b997cd978af7dc
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-03 17:33:11 +0000

    corrections and mains

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -6,6 +6,5 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """learming rate decay"""
-    learning_rate = tf.train.inverse_time_decay(alpha, global_step,
-                                                decay_step, decay_rate)
-    return (learning_rate)
+    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                       decay_rate, staircase=True)
