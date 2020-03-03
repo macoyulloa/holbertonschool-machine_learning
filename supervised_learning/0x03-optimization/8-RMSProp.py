62d252b0f5458860f8f61f8d9541af95e761f927
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-03 18:26:50 +0000

    correction poiont 8

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -6,5 +6,6 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """gradient descent RMSProp optimization algorithm"""
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon).minimize(loss)
-    return (optimizer)
+    return tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                     decay=beta2,
+                                     epsilon=epsilon).minimize(loss)
