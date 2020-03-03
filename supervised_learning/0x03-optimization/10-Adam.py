66e2f0a072ab34b6d754e7c14d5878cd3be4def6
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-03 01:38:45 +0000

    Adam optimization

---
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Optimization tasks"""
+
+import numpy as np
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """Adam optimization algorithm"""
+    optim = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).miniize(loss)
+    return (optim)
