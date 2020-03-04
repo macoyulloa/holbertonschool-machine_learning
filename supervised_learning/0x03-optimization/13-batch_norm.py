f03c206beafdc3681713911e3800537bba829053
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-04 14:15:57 +0000

    corrections

---
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Optimization tasks"""
+
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """batch normalization"""
+    mean = Z.mean(axis=0)
+    var = Z.var(axis=0)
+
+    std = var + epsilon
+    Z_centered = Z - mean
+    Z_norm = Z_centered / std
+    Z_n_batch = (gamma * Z_norm) + beta
+    return Z_n_batch
--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -7,8 +7,6 @@ import tensorflow as tf
 def create_batch_norm_layer(prev, n, activation):
     """batch normalization using tensorflow"""
     layer = tf.layers.Dense(units=n, activation=activation)
-    mean, variance = tf.nn.moments(layer(prev), axes=[0])
-    norm = tf.nn.batch_normalization(layer(prev), mean=mean,
-                                     variance=variance, offset=None,
-                                     scale=None, variance_epsilon=1e-3)
+    m, var = tf.nn.moments(layer(prev), axes=[0])
+    norm = tf.nn.batch_normalization(layer(prev), mean=m, variance=var, offset=None, scale=None, variance_epsilon=1e-8)
     return norm
