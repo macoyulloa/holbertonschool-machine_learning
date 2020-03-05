5c99818327c653cf18bdcbaa7fd9e0c454f7a2eb
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-05 21:52:18 +0000

    corrections

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -8,10 +8,7 @@ def sensitivity(confusion):
     """ Calculate the sensitive of each class
     return the sensitivy of each class
     """
-    sensitivity_list = []
     cond_positive = np.sum(confusion, axis=1)
-    for i in range(len(confusion)):
-        for j in range(len(confusion)):
-            if i == j:
-                sensitivity_list.append(confusion[i][j]/cond_positive[i])
-    return (sensitivity_list)
+    true_pos = np.diagonal(confusion)
+    sensitivity = true_pos / cond_positive
+    return (sensitivity)
--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -8,10 +8,7 @@ def precision(confusion):
     """ Calculate the precision of each class
     return the precision of each class
     """
-    precision_list = []
     predic_cond_pos = np.sum(confusion, axis=0)
-    for i in range(len(confusion)):
-        for j in range(len(confusion)):
-            if i == j:
-                precision_list.append(confusion[i][j]/predic_cond_pos[i])
-    return (precision_list)
+    true_pos = np.diagonal(confusion)
+    precision = true_pos / predic_cond_pos
+    return (precision)
