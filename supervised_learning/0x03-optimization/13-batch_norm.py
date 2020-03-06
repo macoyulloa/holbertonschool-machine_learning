315aa7d8b58938ce4240c4eec759315e8df32a2c
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-06 01:33:22 +0000

    correction

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -7,9 +7,9 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """batch normalization"""
     mean = Z.mean(axis=0)
-    var = np.sqrt(Z.var(axis=0))
+    var = Z.var(axis=0)
 
-    std = var + epsilon
+    std = np.sqrt(var + epsilon)
     Z_centered = Z - mean
     Z_norm = Z_centered / std
     Z_n_batch = (gamma * Z_norm) + beta
