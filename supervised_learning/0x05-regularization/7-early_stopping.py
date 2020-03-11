237a842986c2029c6c25f3a180f6ccdd4bf337c5
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-11 18:07:09 +0000

    early stopping

---
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Early stopping regularization of a model"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Know if you should stop gradient descent
+    cost: validation cost of the neural
+    opt_cost: lowest recorded validation cost
+    threshold: threshold used for stoping
+    patience: used for early stopping
+    count: how long the thredshold has not been met
+    return: boolean
+    """
+    if (opt_cost - cost) > threshold:
+        count = 0
+    else:
+        count += 1
+    if (count == 15):
+        boolean = True
+    else:
+        boolean = False
+    return (boolean, count)
