33b82c6c7d5f73952161d036f6455259cf48ac97
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-13 20:23:21 +0000

    building a modle in keras

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""building a deep learning model using keras"""
+import tensorflow as tf
+from tensorflow import keras
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    nx: number of features
+    layers: list with the nodes per layer
+    activations: active function per layer
+    lambtha: l2 rate 0.001
+    keep_prob: to keep the node for dropout
+    """
+
+    model = tf.keras.Sequential()
+    reg_l2 = tf.keras.regularizers.l2(lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(keras.layers.Dense(layers[i], input_shape=(nx,),
+                                         activation=activations[i],
+                                         kernel_regularizer=reg_l2))
+        else:
+            model.add(keras.layers.Dropout(keep_prob))
+            model.add(keras.layers.Dense(layers[i],
+                                         activation=activations[i],
+                                         kernel_regularizer=reg_l2))
+    return model
