27236ae4c5461cb18ac779347d6bd1ae3d816be2
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-15 02:04:56 +0000

    uploading code since point 11

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""building a deep learning model using keras"""
+import tensorflow as tf
+
+
+def predict(network, data, verbose=False):
+    """
+    Arg:
+        network: neural model
+        data: inputs
+        verbose: determine if output should be printed
+    Return: prediction for the data
+    """
+    return network.predict(data, verbose=verbose)
