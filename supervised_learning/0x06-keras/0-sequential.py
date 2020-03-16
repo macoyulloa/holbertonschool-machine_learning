4c52fa0707d7d0c5f92af7271e21013926c57214
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-16 02:26:33 +0000

    sequestial

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -18,10 +18,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         if i == 0:
             model.add(K.layers.Dense(layers[i], input_shape=(nx,),
                                      activation=activations[i],
-                                     kernel_regularizer=reg_l2))
+                                     kernel_regularizer=reg_l2,
+                                     name='dense'))
         else:
             model.add(K.layers.Dropout(keep_prob))
             model.add(K.layers.Dense(layers[i],
                                      activation=activations[i],
-                                     kernel_regularizer=reg_l2))
+                                     kernel_regularizer=reg_l2,
+                                     name='dense_' + str(i)))
     return model
