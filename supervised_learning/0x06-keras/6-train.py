c18939d3e1398001e5629a0d081300181163a3db
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-16 18:48:22 +0000

    checkers

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -20,11 +20,18 @@ def train_model(network, data, labels, batch_size, epochs,
         shuffle: determines whether to shuffle
     return: History object generated
     """
-    es = K.callbacks.EarlyStopping(monitor='val_loss',
-                                   patience=patience)
-    history = network.fit(x=data, y=labels, epochs=epochs,
-                          batch_size=batch_size,
-                          validation_data=validation_data,
-                          verbose=verbose, shuffle=shuffle,
-                          callbacks=[es])
+    if validation_data:
+        es = K.callbacks.EarlyStopping(monitor='val_loss',
+                                       patience=patience)
+        history = network.fit(x=data, y=labels, epochs=epochs,
+                              batch_size=batch_size,
+                              validation_data=validation_data,
+                              verbose=verbose, shuffle=shuffle,
+                              callbacks=[es])
+    else:
+        history = network.fit(x=data, y=labels, epochs=epochs,
+                              batch_size=batch_size,
+                              validation_data=validation_data,
+                              verbose=verbose, shuffle=shuffle,
+                              callbacks=None)
     return history
