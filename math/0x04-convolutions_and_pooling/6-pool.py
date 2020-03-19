826182e902d952ffdd0f82bf24e63a2bc8289da4
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-19 04:23:04 +0000

    pool methods

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -13,24 +13,24 @@ def pool(images, kernel_shape, stride, mode='max'):
     Return: padded convolved images RGB np.array
     """
     m, img_h, img_w, img_c = images.shape
-    k_h, k_w  = kernel_shape.shape
-
-    if mode == 'max':
-        
-    if mode == 'avg':
-        
+    k_h = kernel_shape[0]
+    k_w = kernel_shape[1]
 
     out_h = int(((img_h - k_h) / (stride[0])) + 1)
     out_w = int(((img_w - k_w) / (stride[1])) + 1)
-
     output_conv = np.zeros((m, out_h, out_w, img_c))
     m_img = np.arange(0, m)
 
     for i in range(out_h):
         for j in range(out_w):
-            output_conv[m_img, i, j, f] = np.sum(np.multiply(
-                images[m_img,
-                       i*(stride[0]):k_h+(i*(stride[0])),
-                       j*(stride[1]):k_w+(j*(stride[1]))],
-                kernel_shape), axis=(1, 2, 3))
+            if mode == 'max':
+                output_conv[m_img, i, j] = np.max(
+                    images[m_img,
+                           i*(stride[0]):k_h+(i*(stride[0])),
+                           j*(stride[1]):k_w+(j*(stride[1]))], axis=(1, 2))
+            if mode == 'avg':
+                output_conv[m_img, i, j] = np.mean(
+                    images[m_img,
+                           i*(stride[0]):k_h+(i*(stride[0])),
+                           j*(stride[1]):k_w+(j*(stride[1]))], axis=(1, 2))
     return output_conv
