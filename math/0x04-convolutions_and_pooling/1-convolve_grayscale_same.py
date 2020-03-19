e9731e87114df304ea20809f1677cb66be7e860f
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-19 21:48:11 +0000

    managing cases

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -13,16 +13,25 @@ def convolve_grayscale_same(images, kernel):
     m = images.shape[0]
     img_h = images.shape[1]
     img_w = images.shape[2]
-    k_h = kernel.shape[0]
-    k_w = kernel.shape[1]
+    k_h = int(kernel.shape[0])
+    k_w = int(kernel.shape[1])
 
-    p = int((k_h - 1) / 2)
-    out_h = img_h - k_h + 1 + (2*p)
-    out_w = img_w - k_w + 1 + (2*p)
+    if (k_h % 2) == 0:
+        p_h = int((k_h) / 2)
+    else:
+        p_h = int((k_h - 1) / 2)
+
+    if (k_w % 2) == 0:
+        p_w = int((k_w) / 2)
+    else:
+        p_w = int((k_w - 1) / 2)
+
+    out_h = img_h - k_h + 1 + (2*p_h)
+    out_w = img_w - k_w + 1 + (2*p_w)
     output_conv = np.zeros((m, out_h, out_w))
 
     m_img = np.arange(0, m)
-    images = np.pad(images, [(0, 0), (p, p), (p, p)],
+    images = np.pad(images, [(0, 0), (p_h, p_h), (p_w, p_w)],
                     'constant', constant_values=0)
 
     for i in range(out_h):
