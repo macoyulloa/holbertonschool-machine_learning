2a43447aebaeaa77a0e315fc83d585445bd28e67
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-20 00:02:03 +0000

    same case correction

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -17,14 +17,15 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     img_w = images.shape[2]
     k_h = kernel.shape[0]
     k_w = kernel.shape[1]
+    s_h, s_w = stride
 
     if padding == 'valid':
         p_h = 0
         p_w = 0
 
     if padding == 'same':
-        p_h = int((k_h - 1) / 2)
-        p_w = int((k_h - 1) / 2)
+        p_h = int(((s_h*img_h) - s_h + k_h - img_h) / 2) + 1
+        p_w = int(((s_w*img_w) - s_w + k_w - img_w) / 2) + 1
 
     if type(padding) == tuple:
         p_h = padding[0]
@@ -35,7 +36,6 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     out_h = int(((img_h - k_h + (2*p_h)) / (stride[0])) + 1)
     out_w = int(((img_w - k_w + (2*p_w)) / (stride[1])) + 1)
-
     output_conv = np.zeros((m, out_h, out_w))
     m_img = np.arange(0, m)
 
