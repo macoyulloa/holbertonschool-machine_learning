971e7a7ad7f2f8e7c610eb85b0fbad849c07ced0
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-03-24 20:48:53 +0000

    correction

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -16,25 +16,24 @@ def lenet5(x, y):
         a tensor for the loss of the netowrk
         a tensor for the accuracy of the network
     """
-    k_init = tf.contrib.layers.variance_scaling_initializer(
-        mode="FAN_AVG")
+    k_init = tf.contrib.layers.variance_scaling_initializer()
     activation = tf.nn.relu
 
-    layer_1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+    layer_1 = tf.layers.Conv2D(filters=6, kernel_size=5,
                                padding='same',
                                activation=activation,
                                kernel_initializer=k_init)(x)
 
     pool_1 = tf.layers.MaxPooling2D(pool_size=[2, 2],
-                                    strides=(2, 2))(layer_1)
+                                    strides=2)(layer_1)
 
-    layer_2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+    layer_2 = tf.layers.Conv2D(filters=16, kernel_size=5,
                                padding='valid',
                                activation=activation,
                                kernel_initializer=k_init)(pool_1)
 
     pool_2 = tf.layers.MaxPooling2D(pool_size=[2, 2],
-                                    strides=(2, 2))(layer_2)
+                                    strides=2)(layer_2)
 
     flatten = tf.layers.Flatten()(pool_2)
 
@@ -50,12 +49,12 @@ def lenet5(x, y):
     y_pred = tf.nn.softmax(output_layer)
 
     loss = tf.losses.softmax_cross_entropy(
-        y, y_pred, reduction=tf.losses.Reduction.MEAN)
+        y, output_layer, reduction=tf.losses.Reduction.MEAN)
 
     train = tf.train.AdamOptimizer().minimize(loss)
 
     equality = tf.equal(tf.argmax(y, axis=1),
-                        tf.argmax(y_pred, axis=1))
+                        tf.argmax(output_layer, axis=1))
     acc = tf.reduce_mean(tf.cast(equality, tf.float32))
 
     return y_pred, train, loss, acc
