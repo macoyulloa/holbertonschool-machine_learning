90f0c1382e74e13074e740cb5c918036f1082e79
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-03 01:16:26 +0000

    pool backward, average edge casa

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -14,8 +14,25 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Returns: parcial dev prev layer (dA_prev)
     """
     k_h, k_w = kernel_shape
+    _, h_new, w_new, c_new = dA.shape
     m, h_x, w_x, c_prev = A_prev.shape
     s_h, s_w = stride
 
-    dx = np.zeros_like(A_prev, dtype=float)
+    dx = np.zeros_like(A_prev)
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for f in range(c_new):
+                    if mode == 'max':
+                        dx[i,
+                           h*(stride[0]):(h*(stride[0]))+k_h,
+                           w*(stride[1]):(w*(stride[1]))+k_w,
+                           :] = dA[i, h, w, f]
+                    if mode == 'avg':
+                        dx[i,
+                           h*(stride[0]):(h*(stride[0]))+k_h,
+                           w*(stride[1]):(w*(stride[1]))+k_w,
+                           :] = dA[i, h, w, f]
+
     return dx
