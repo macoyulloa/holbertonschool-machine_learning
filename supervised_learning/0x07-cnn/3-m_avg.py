f5425b59c58693d2b66cc35b6272398b5f40706e
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-08 03:34:32 +0000

    mains

---
+++ supervised_learning/0x07-cnn/3-m_avg.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+pool_backward = __import__('3-pool_backward').pool_backward
+
+np.random.seed(7)
+m = np.random.randint(100, 200)
+h, w = np.random.randint(20, 50, 2).tolist()
+c = np.random.randint(2, 5)
+fh, fw = (np.random.randint(2, 5, 2)).tolist()
+sh, sw = (np.random.randint(2, 5, 2)).tolist()
+
+X = np.random.uniform(0, 1, (m, h, w, c))
+dA = np.random.uniform(0, 1, (m, (h - fh) // sh + 1, (w - fw) // sw + 1, c))
+Y = pool_backward(dA, X, (fh, fw), stride=(sh, sw), mode='avg')
+print(Y)
+print(Y.shape)
