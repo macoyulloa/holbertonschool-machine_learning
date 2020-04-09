69c3cbf033ba706194a858a495108decac57c24c
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-09 02:51:21 +0000

    correction

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -2,55 +2,90 @@
 """Transfer learning"""
 
 import tensorflow.keras as K
+from tensorflow.keras import Sequential
+from tensorflow.keras.applications import VGG19,ResNet50
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD,Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.layers import Flatten,Dense,BatchNormalization,Activation,Dropout
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import cifar10
 
 
 if __name__ == '__main__':
 
+    base_model = K.applications.resnet50.ResNet50(weights='imagenet',
+                                                  include_top=False,
+                                                  input_shape=(32, 32, 3))
+
     (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
 
-    x_train = K.applications.xception.preprocess_input(x_train)
-    x_test = K.applications.xception.preprocess_input(x_test)
+    x_train = K.applications.resnet50.preprocess_input(x_train)
+    x_test = K.applications.resnet50.preprocess_input(x_test)
 
     y_train = K.utils.to_categorical(y_train, 10)
     y_test = K.utils.to_categorical(y_test, 10)
 
-    print(x_train.shape)
-    print(x_test.shape)
+    train_generator = ImageDataGenerator(
+        rotation_range=2,
+        horizontal_flip=True,
+        zoom_range=.1)
 
-    base_model = K.applications.xception.Xception(include_top=False,
-                                                  input_shape=(128, 128, 3),
-                                                  weights='imagenet')
+    test_generator = ImageDataGenerator(
+        rotation_range=2,
+        horizontal_flip= True,
+        zoom_range=.1)
 
-    # Top Model Block
-    model = K.models.Sequential()
-    model.add(K.layers.UpSampling2D((2, 2)))
-    model.add(K.layers.UpSampling2D((2, 2)))
-    model.add(base_model)
-    model.add(K.layers.GlobalAveragePooling2D())
-    model.add(K.layers.Dense(10, activation='softmax'))
+    train_generator.fit(x_train)
+    test_generator.fit(x_test)
 
-    # first: train only the top layers (which were randomly initialized)
-    # i.e. freeze all layers of the based model that is already pre-trained.
-    for layer in base_model.layers:
-        layer.trainable = False
+    lrr= ReduceLROnPlateau(
+        monitor='val_acc',
+        factor=.01,
+        patience=3,
+        min_lr=1e-5
+    )
 
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['acc'])
+    base_model_1 = VGG19(include_top=False,
+                         weights='imagenet',
+                         input_shape=(32,32,3),
+                         classes=y_train.shape[1])
 
-    checkpoint = K.callbacks.ModelCheckpoint('cifar10.h5',
-                                             monitor='val_acc',
-                                             save_best_only=True)
+    base_model_2 = ResNet50(include_top=False,
+                            weights='imagenet',
+                            input_shape=(32,32,3),
+                            classes=y_train.shape[1])
 
-    history = model.fit(x_train,
-                        y_train,
-                        epochs=15,
-                        batch_size=256,
-                        callbacks=[checkpoint],
-                        validation_data=(x_test, y_test))
+    model_1= Sequential()
+    model_1.add(base_model_2)
+    model_1.add(Flatten())
+    model_1.add(Dense(10,activation=('softmax')))
+
+    model_1.summary()
+
+    batch_size= 100
+    epochs=4
+    learn_rate=.001
+
+    sgd=SGD(lr=learn_rate,momentum=.9,nesterov=False)
+    adam=Adam(lr=learn_rate, beta_1=0.9,
+              beta_2=0.999, epsilon=None,
+              decay=0.0, amsgrad=False)
+
+    model_1.compile(optimizer=sgd,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+    history = model_1.fit_generator(train_generator.flow(x_train,
+                                                         y_train,
+                                                         batch_size=batch_size),
+                                    epochs=epochs,
+                                    steps_per_epoch=x_train.shape[0]//batch_size,
+                                    callbacks=[lrr],
+                                    verbose=1,
+                                    validation_data=(x_test, y_test))
 
     model.save('cifar10.h5')
-
 
 def preprocess_data(X, Y):
     """pre-processes the data model
@@ -63,6 +98,6 @@ def preprocess_data(X, Y):
         Y_p is a numpy.ndarray preprocessed Y
     Notes: m is the number of data points
     """
-    X = K.applications.xception.preprocess_input(X)
+    X = K.applications.resnet50.preprocess_input(X)
     Y = K.utils.to_categorical(Y, 10)
     return X, Y
