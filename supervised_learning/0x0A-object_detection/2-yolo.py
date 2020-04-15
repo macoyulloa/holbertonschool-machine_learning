236f1acaea0404f157b14ffb67a2bc71ddfdc43c
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-15 23:14:12 +0000

    first version of boxes classification

---
+++ supervised_learning/0x0A-object_detection/2-yolo.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+"""Yolo algorithm construction"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo():
+    """class Yolo that uses the Yolo v3 algorithm to perform object detection
+    """
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """ initialization of the variables
+        Arg:
+            model_path: path to where a Darknet Keras model is stored
+            classes_path: path with the list of class names used,
+                          listed in order of index, can be found
+            class_t: float, box score threshold for the init filtering step
+            nms_t: float representing the IOU threshold for non-max suppression
+            anchors: np.ndarray.shape (outputs, anchor_boxes, 2)
+                     has all anchor boxes:
+                     outputs: number of outputs (predictions)
+                     anchor_boxes: num of anchor boxes used for each prediction
+                     2 => [anchor_box_width, anchor_box_height]
+        """
+        class_names = []
+        model = K.models.load_model(model_path)
+        with open(classes_path, "r") as classes_file:
+            class_names = classes_file.readlines()
+        class_names = [x.strip() for x in class_names]
+
+        self.model = model
+        self.class_names = class_names
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def process_outputs(self, outputs, image_size):
+        """ process the outputs
+        Arg:
+            outputs: list of numpy.ndarrays containing the predictions:
+                Each output will have the shape
+                (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
+                     grid_height & grid_width =>
+                     the height and width of the grid used for the output
+                     anchor_boxes => the number of anchor boxes used
+                     4 => (t_x, t_y, t_w, t_h)
+                     1 => box_confidence
+                     classes => class probabilities for all classes
+            image_size: numpy.ndarray image’s original size [height, width]
+        Returns: tuple of (boxes, box_confidences, box_class_probs):
+                 boxes: np shape (grid_height, grid_width, anchor_boxes, 4)
+                 box_confidences: np.shape (grid_height, grid_width,
+                    anchor_boxes, 1) has box confidences for each output
+                 box_class_probs:(grid_height, grid_width, anchor_boxes,
+                    classes) box’s class probabilities for each output
+        """
+        boxes = []
+        box_confidence = []
+        box_class_probs = []
+
+        for i in range(len(outputs)):
+            grid_h, grid_w, nb_box, _ = outputs[i].shape
+
+            box_conf = 1 / (1 + np.exp(-(outputs[i][:, :, :, 4:5])))
+            box_confidence.append(box_conf)
+            box_prob = 1 / (1 + np.exp(-(outputs[i][:, :, :, 5:])))
+            box_class_probs.append(box_prob)
+
+            box_xy = 1 / (1 + np.exp(-(outputs[i][:, :, :, :2])))
+            box_wh = np.exp(outputs[i][:, :, :, 2:4])
+            anchors_tensor = self.anchors.reshape(1, 1,
+                                                  self.anchors.shape[0],
+                                                  nb_box, 2)
+            box_wh = box_wh * anchors_tensor[:, :, i, :, :]
+
+            col = np.tile(np.arange(0, grid_w), grid_w).reshape(-1, grid_w)
+            row = np.tile(np.arange(0, grid_h).reshape(-1, 1), grid_h)
+            col = col.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+            row = row.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+            grid = np.concatenate((col, row), axis=-1)
+
+            box_xy += grid
+            box_xy /= (grid_w, grid_h)
+            input_h = self.model.input.shape[1].value
+            input_w = self.model.input.shape[2].value
+            box_wh /= (input_w, input_h)
+            box_xy -= (box_wh / 2)
+            box_xy1 = box_xy
+            box_xy2 = box_xy1 + box_wh
+            box = np.concatenate((box_xy1, box_xy2), axis=-1)
+
+            box[..., 0] = (box[..., 0] * image_size[1])
+            box[..., 2] = (box[..., 2] * image_size[1])
+            box[..., 1] = (box[..., 1] * image_size[0])
+            box[..., 3] = (box[..., 3] * image_size[0])
+
+            boxes.append(box)
+
+        return ((boxes, box_confidence, box_class_probs))
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """ filtering the boxes with object threshold
+        Arg:
+            boxes: np shape (grid_height, grid_width, anchor_boxes, 4)
+                   with the process outputs boxes
+            box_confidences: shape (grid_height, grid_width, anchor_boxes, 1)
+                   has all the processed box confidences
+            box_class_probs: (grid_height, grid_width, anchor_boxes, classes)
+                   has all the processed box class probabilities
+        Returns tuple of (filtered_boxes, box_classes, box_scores)
+            filtered_boxes: np array shape (?, 4) all filtered bounding boxes
+            box_classes: np array shape (?,) class number that each box in
+                         filtered_boxes predicts
+            box_scores: np array of shape (?) the box scores for each box
+                        in filtered_boxes
+        """
+        filtered_boxes = []
+        box_classes = []
+        box_scores = []
+
+        for i in range(len(boxes)):
+            box_score = box_confidences[i] * box_class_probs[i]
+            box_class_score = np.max(box_score, axis=-1)
+            box_class = np.argmax(box_score, axis=-1)
+            filt = np.where(box_class_score >= self.class_t)
+
+            box = boxes[i][filt]
+            filtered_boxes.append(box)
+
+            box_score = box_class_score[filt]
+            box_scores.append(box_score)
+
+            classes = box_class[filt]
+            box_classes.append(classes)
+
+        return (filtered_boxes, box_classes, box_scores)
