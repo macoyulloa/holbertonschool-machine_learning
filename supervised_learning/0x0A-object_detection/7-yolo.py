1871cb43d0a63e4630bac6cc91eeb4e343c25875
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-20 02:50:27 +0000

    correction process image

--- supervised_learning/0x0A-object_detection/7-yolo.py
+++ supervised_learning/0x0A-object_detection/7-yolo.py
@@ -243,21 +243,18 @@ class Yolo():
                           2 => (image_height, image_width)
         """
         images_dimensions = [img.shape[:2] for img in images]
-        images_shapes = np.stack(images_dimensions)
+        images_shapes = np.stack(images_dimensions, axis=0)
 
         input_h = self.model.input.shape[1].value
         input_w = self.model.input.shape[2].value
-        resized_images = []
 
-        for i, img in enumerate(images):
-            resized_image = cv2.resize(img, (input_h, input_w),
-                                       interpolation=cv2.INTER_CUBIC)
-            resized_image = resized_image.astype('float32')
-            max_pixel = resized_image.max()
-            resized_image /= max_pixel
-            resized_images.append(resized_image)
+        images_resized = [cv2.resize(img,
+                                     (input_h, input_w),
+                                     interpolation=cv2.INTER_CUBIC)
+                          for img in images]
+        images_rescaled = [img/255 for img in images_resized]
 
-        pimages = np.stack(resized_images)
+        pimages = np.stack(images_rescaled, axis=0)
 
         return (pimages, images_shapes)
 
