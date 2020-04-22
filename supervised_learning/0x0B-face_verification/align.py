198e00246ddbd155bb44a3b8a176242616b2582b
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-22 21:42:48 +0000

    detecting the face with dlib

--- supervised_learning/0x0B-face_verification/align.py
+++ supervised_learning/0x0B-face_verification/align.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-""" face verification system """
+"""face detection/verification system"""
 
 import dlib
+import numpy as np
+import cv2
 
 
 class FaceAlign():
+    """class FaceAlign to permorfs a face verifications system
+    """
+
     def __init__(self, shape_predictor_path):
         """ initializing the variables of the model
         Arg:
@@ -18,3 +23,33 @@ class FaceAlign():
 
         self.detector = detector
         self.shape_predictor = shape_predictor
+
+    def detect(self, image):
+        """ detects a face in an image
+        Arg:
+            image: np.ndarray containing an image from which to detect a face
+        Returns: dlib.rectangle containing the boundary box for the face
+                 in the image, or None on failure
+               - If multiple faces are detected, return the largest area rec
+               - If no faces, return dlib.rect that is the same as the image
+        """
+        faces = self.detector(image, 1)
+        if not faces:
+            return None
+        maxArea = 0
+
+        if len(faces) == 0:
+            # non faces was detected
+            rectanlge_face = dlib.rectangle(left=0,
+                                            top=0,
+                                            right=image.shape[1],
+                                            bottom=image.shape[0])
+        if len(faces) >= 1:
+            # if one face was detected: rectangle_face = faces[0]
+            # multiple faces was detected, take the max box
+            for (i, rect) in enumerate (faces):
+                if rect.area() > maxArea:
+                    maxArea = rect.area()
+                    rectangle_face = rect
+
+        return rectangle_face
