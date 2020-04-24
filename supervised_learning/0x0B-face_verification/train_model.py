55f9f09aa13be69d90249d1521225f80add2dc5f
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-24 19:21:35 +0000

    trains the model functions, accuracy and save the model

--- supervised_learning/0x0B-face_verification/train_model.py
+++ supervised_learning/0x0B-face_verification/train_model.py
@@ -24,13 +24,47 @@ class TrainModel():
         """
         with tf.keras.utils.CustomObjectScope({'tf': tf}):
             self.base_model = tf.keras.models.load_model(model_path)
-
         self.alpha = alpha
-
-        tl = TripletLoss(0.2)
-        output = tl(inputs)
-        training_model = tf.keras.models.Model([A, P, N], output)
-
+        # create the new model: training model
+        inputs = [A, P, N]
+        output = TripletLoss(base_model.output)
+        training_model = tf.keras.models.Model(inputs, output)
         training_model.compile(optimizer='Adam')
-
         training_model.save()
+
+        def train(self, triplets, epochs=5, batch_size=32,
+                  validation_split=0.3, verbose=True):
+            """training method
+            Arg:
+            - triplets: list containing the inputs to self.training_model
+            - epochs: number of epochs to train for
+            - batch_size: is the batch size for training
+            - validation_split: is the validation split for training
+            - verbose: is a boolean that sets the verbosity mode
+            Returns: the History output from the training
+            """
+            history = self.training_model.fit(
+                x_t, y_train_oh,
+                validation_split=validation_split
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=verbose)
+            return history
+
+        def save(self, save_path):
+            """saves the base embedding model
+            Arg:
+                save_path: path save the model
+            Returns: the saved model
+            """
+            return self.training_model.save(save_path)
+
+        @staticmethod
+        def f1_score(y_true, y_pred):
+            """ define the f1 score """
+            return f1
+
+        @staticmethod
+        def accuracy(y_true, y_pred):
+            """ define the accuracy """
+            return acc
