88d6d05c71604c1ba275864f435a81f56c89be6c
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-24 05:18:11 +0000

    creting the model in my layer

--- supervised_learning/0x0B-face_verification/triplet_loss.py
+++ supervised_learning/0x0B-face_verification/triplet_loss.py
@@ -28,10 +28,9 @@ class TripletLoss(tensorflow.keras.layers.Layer):
         anchor_output, positive_output, negative_output = inputs
         d_pos = tf.reduce_sum(tf.square(anchor_output - positive_output), 1)
         d_neg = tf.reduce_sum(tf.square(anchor_output - negative_output), 1)
-
-        min_value = tf.convert_to_tensor(0.0, dtype=tf.float64)
-        loss = tf.maximum(min_value, self.alpha + d_pos - d_neg)
-
+        triplet_values = self.alpha + d_pos - d_neg
+        loss = tf.maximum(0.0, tf.cast(triplet_values, tf.float32))
+        loss = tf.cast(loss, tf.float32)
         return loss
 
     def call(self, inputs):
@@ -41,4 +40,4 @@ class TripletLoss(tensorflow.keras.layers.Layer):
                     output tensors from the last layer of the model
         return: the triplet loss tensor
         """
-        return K.(inputs, self.loss)
+        return (self.triplet_loss(inputs))
