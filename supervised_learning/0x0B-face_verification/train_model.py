22d827c59837052ef80f695cbc1b68b9d487eb62
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-04-25 00:05:21 +0000

    f1 method and acc

--- supervised_learning/0x0B-face_verification/train_model.py
+++ supervised_learning/0x0B-face_verification/train_model.py
@@ -65,9 +65,15 @@ class TrainModel():
         @staticmethod
         def f1_score(y_true, y_pred):
             """ define the f1 score """
-            return f1
+            true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+            possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+            predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+            precision = true_positives / (predicted_positives + K.epsilon())
+            recall = true_positives / (possible_positives + K.epsilon())
+            f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+            return f1_val
 
         @staticmethod
         def accuracy(y_true, y_pred):
             """ define the accuracy """
-            return acc
+            return roc_auc_score(yprobs, probs)
