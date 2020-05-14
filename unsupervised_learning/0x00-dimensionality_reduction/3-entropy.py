0f777e753b09a3957b91ce949b8ea5bd64671199
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-14 00:35:31 +0000

    Calculates the entropy and P affinties

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""t-SNE method"""
+
+import numpy as np
+
+
+def HP(Di, beta):
+    """calculates the Shannon entropy & P affinities relative to a data point:
+    Arg:
+      - Di: np.ndarray (n - 1,) pariwise distances between a data point
+           and all other points
+         n is the number of data points
+      - beta: is the beta value for the Gaussian distribution
+
+    Returns: (Hi, Pi)
+      - Hi: is the Shannon entropy of the points
+      - Pi: np.ndarray (n - 1,) contain the P affinities of the points
+    """
+    Pi = (np.exp(-Di * beta)) / (np.sum(np.exp(-Di * beta)))
+
+    H = - np.sum(Pi * np.log2(Pi))
+    Hi = 2**H
+
+    return (Hi, Pi)
