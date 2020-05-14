3727a8e09c3048a9b18642523e1b770589e13112
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-14 19:38:49 +0000

    pycodestyle correction

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -15,18 +15,15 @@ def P_init(X, perplexity):
     Returns: (D, P, betas, H)
       - D: np.ndarray (n, n) calculates pairwise distance between data points
       - P: np.ndarray (n, n) initialized 0‘s that will contain the P affinities
-      - betas: np.ndarray (n, 1) initialized to all 1’s that contain beta values
+      - betas: np.ndarray (n, 1) initialized 1’s that contain beta values
       - H is the Shannon entropy for perplexity perplexity
     """
     n, d = X.shape
 
     sum_X = np.sum(np.square(X), axis=1)
     D = (np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X))
-
-    P = np.zeros([n, n], dtype = 'float64')
-
-    betas = np.ones([n, 1], dtype = 'float64')
-
+    P = np.zeros([n, n], dtype='float64')
+    betas = np.ones([n, 1], dtype='float64')
     H = np.log2(perplexity)
 
     return (D, P, betas, H)
