31919e464a07fd754ff4198aae6bed87bc5d7e6e
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-16 21:57:15 +0000

    exeptions change

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -16,16 +16,15 @@ def likelihood(x, n, P):
        - 1D: with the likelihood of obtaining the data, x and n, for
              each probability in P, respectively
     """
-    if (not isinstance(n, int)) or (n < 0):
+    if (type(n) != int) and (n < 0):
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or (n <= 0):
+    if (type(x) != int) and (n <= 0):
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if (x > n):
         raise ValueError("x cannot be greater than n")
-    if (len(P.shape) != 1) or (type(P) != np.ndarray):
+    if (len(P.shape) != 1) and (type(P) != np.ndarray):
         raise TypeError("P must be a 1D numpy.ndarray")
-
     if (np.any((np.vectorize(lambda x: 0 <= x <= 1)(P)) is False)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
