bed20b8d4dd941ceaf3f6447663b5ad051da2212
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-19 03:11:25 +0000

    gradients

--- unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -22,10 +22,13 @@ def grads(Y, P):
 
     Q, num = Q_affinities(Y)
     pq_diff = P - Q
-    pq_diff_num = np.expand_dims((pq_diff * num).T, axis=-1)
+    # pq_diff_num = np.expand_dims((pq_diff * num).T, axis=-1)
     dY = np.zeros([n, ndim])
     for i in range(n):
         Y_diff = Y[i, :] - Y
-        dY[i, :] = np.sum((pq_diff_num[i, :] * Y_diff), axis=0)
+        # dY[i, :] = np.sum((pq_diff_num[i, :] * Y_diff), axis=0)
+        part1 = np.expand_dims((pq_diff[:, i] * num[:, i]).T, axis=-1)
+        part2 = (Y[i, :] - Y)
+        dY[i, :] = np.sum(part1 * part2, axis=0)
 
     return (dY, Q)
