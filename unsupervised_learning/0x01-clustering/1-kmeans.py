dce3070180c80ab382520047b7271085b0f072c2
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-20 21:28:32 +0000

    edges cases and failure cases of the function

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -19,35 +19,38 @@ def kmeans(X, k, iterations=1000):
         - clss: np.ndarray (n,) with the index of the cluster in C that
                 each data point belongs to
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
+        return None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None
+
     n, d = X.shape
 
-    try:
-        X_min = X.min(axis=0)
-        X_max = X.max(axis=0)
-        # initialized the centroids points
-        C = np.random.uniform(X_min, X_max, size=(k, d))
-        # clustering my data
-        for i in range(iterations):
-            C_copy = np.copy(C)
-            # distances of the x's between each centroid, k
-            distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
-            # matrix with the centroid per xi
-            clss = np.argmin(distances, axis=0)
+    X_min = X.min(axis=0)
+    X_max = X.max(axis=0)
+    # initialized the centroids points
+    C = np.random.uniform(X_min, X_max, size=(k, d))
+    # clustering my data
+    for i in range(iterations):
+        C_copy = np.copy(C)
+        # distances of the x's between each centroid, k
+        distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
+        # matrix with the centroid per xi
+        clss = np.argmin(distances, axis=0)
 
-            # moving my centroid depends on the average mean based on the
-            # group of x's per cluster
-            for k in range(C.shape[0]):
-                # if not found any x of the centroid need to be reinitialized
-                if (X[clss == k].size == 0):
-                    C[k, :] = np.random.uniform(X_min, X_max, size=(1, d))
+        # moving my centroid depends on the average mean based on the
+        # group of x's per cluster
+        for k in range(C.shape[0]):
+            # if not found any x of the centroid need to be reinitialized
+            if (X[clss == k].size == 0):
+                C[k, :] = np.random.uniform(X_min, X_max, size=(1, d))
                 # look after the group of x's of each cluster per centroid
-                else:
-                    C[k, :] = (X[clss == k].mean(axis=0))
+            else:
+                C[k, :] = (X[clss == k].mean(axis=0))
             # if there is not change in the centroids breaks the cycle
             if (C_copy == C).all():
                 break
 
-        return (C, clss)
-
-    except Exception:
-        return (None, None)
+    return (C, clss)
