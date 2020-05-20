bf26e928d752999118c34bcc099e260363a31d8c
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-20 22:03:00 +0000

    failure cases of the function

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -27,7 +27,7 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None, None
     if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
         return None, None
-    if kmin > kmax:
+    if kmin >= kmax:
         return None, None
     if type(iterations) != int or iterations <= 0:
         return None, None
