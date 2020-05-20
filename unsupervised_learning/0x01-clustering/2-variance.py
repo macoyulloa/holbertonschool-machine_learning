f0e9ebdcfe3d229000cf3ca66bf1185c45a304c3
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-20 21:40:12 +0000

    failure cases of the function

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -13,14 +13,16 @@ def variance(X, C):
     Returns: var, or None on failure
         - var: is the total variance
     """
-    n, d = X.shape
-    try:
-        distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
-        # selectin the minimun distances depends on the number of
-        # clusters, K
-        min_distances = np.min(distances, axis=0)
-        var = np.sum(min_distances ** 2)
-        return var
-
-    except Exception:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
+    if not isinstance(C, np.ndarray) or len(X.shape) != 2:
+        return None
+
+    n, d = X.shape
+
+    distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
+    # selectin the minimun distances depends on the number of
+    # clusters, K
+    min_distances = np.min(distances, axis=0)
+    var = np.sum(min_distances ** 2)
+    return var
