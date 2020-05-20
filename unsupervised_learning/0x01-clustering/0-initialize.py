1e08cbe9933392f40b4c368542e00be37fbe89e9
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-20 21:23:09 +0000

    edges cases and failure cases of the function

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -18,12 +18,12 @@ def initialize(X, k):
     Returns: np.ndarray of shape (k, d)
         - Initialized centroids for each cluster, or None on failure
     """
-    n, d = X.shape
-
-    try:
-        X_min = X.min(axis=0)
-        X_max = X.max(axis=0)
-        return np.random.uniform(X_min, X_max, size=(k, d))
-
-    except Exception:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
+        return None
+
+    n, d = X.shape
+    X_min = X.min(axis=0)
+    X_max = X.max(axis=0)
+    return np.random.uniform(X_min, X_max, size=(k, d))
