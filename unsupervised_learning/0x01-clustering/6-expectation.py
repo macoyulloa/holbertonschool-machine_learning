3fa58df6012a486a46f9d7162edcee1a1ec4c4ca
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-21 17:21:26 +0000

    edge cases

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -26,15 +26,17 @@ def expectation(X, pi, m, S):
         return None, None
     if not isinstance(S, np.ndarray) or len(S.shape) != 3:
         return None, None
-    if np.sum(pi) != 1:
+    if not np.isclose([np.sum(pi)], [1])[0]:
         return None, None
 
     n, d = X.shape
     k = pi.shape[0]
 
+    if k > n:
+        return None, None
     if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
         return None, None
-    if k != m.shape[0] or k != S.shape[0] or k > n:
+    if k != m.shape[0] or k != S.shape[0]:
         return None, None
 
     gauss_p = np.zeros((k, n))
