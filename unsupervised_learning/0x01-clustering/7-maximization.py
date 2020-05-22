213705eb40ae04d187f4fe5f94c31d9dbed8ef08
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-22 18:16:04 +0000

    edge cases

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -19,32 +19,33 @@ def maximization(X, g):
         - S np.ndarray of shape (k, d, d) containing the
                 updated covariance matrices for each cluster
     """
-    try:
-        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            return None, None, None
-        if not isinstance(g, np.ndarray) or len(g.shape) != 2:
-            return None, None, None
-        if X.shape[0] != g.shape[1]:
-            return None, None, None
-
-        n, d = X.shape
-        k, _ = g.shape
-        pi = np.zeros((k,))
-        m = np.zeros((k, d))
-        S = np.zeros((k, d, d))
-
-        for i in range(k):
-            # maximization updating the mean per each cluster
-            m_num = np.sum((g[i, :, np.newaxis] * X), axis=0)
-            m_den = np.sum(g[i], axis=0)
-            m[i] = m_num / m_den
-            # maxization updating of the covarianze matrixes per cluster
-            s_num = np.dot(g[i] * (X - m[i]).T, (X - m[i]))
-            S[i] = s_num / np.sum(g[i])
-            # maximization updating of the prior per each cluster, k
-            pi[i] = np.sum(g[i]) / n
-
-        return (pi, m, S)
-
-    except Exception:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    if X.shape[0] != g.shape[1]:
+        return None, None, None
+
+    n, d = X.shape
+    probs = np.sum(g, axis=0)
+    total_prob = np.ones((n, ))
+    if not np.isclose(probs, total_prob).all():
+        return None, None, None
+
+    k, _ = g.shape
+    pi = np.zeros((k,))
+    m = np.zeros((k, d))
+    S = np.zeros((k, d, d))
+
+    for i in range(k):
+        # maximization updating the mean per each cluster
+        m_num = np.sum((g[i, :, np.newaxis] * X), axis=0)
+        m_den = np.sum(g[i], axis=0)
+        m[i] = m_num / m_den
+        # maxization updating of the covarianze matrixes per cluster
+        s_num = np.dot(g[i] * (X - m[i]).T, (X - m[i]))
+        S[i] = s_num / np.sum(g[i])
+        # maximization updating of the prior per each cluster, k
+        pi[i] = np.sum(g[i]) / n
+
+    return (pi, m, S)
