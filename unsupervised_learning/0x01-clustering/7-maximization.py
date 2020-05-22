78d72001fc8c45c8fca111d212bb6636701bb84a
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-05-22 18:10:46 +0000

    edge cases

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -26,21 +26,25 @@ def maximization(X, g):
     if X.shape[0] != g.shape[1]:
         return None, None, None
 
-    n, d = X.shape
-    k, _ = g.shape
-    pi = np.zeros((k,))
-    m = np.zeros((k, d))
-    S = np.zeros((k, d, d))
+    try:
+        n, d = X.shape
+        k, _ = g.shape
+        pi = np.zeros((k,))
+        m = np.zeros((k, d))
+        S = np.zeros((k, d, d))
 
-    for i in range(k):
-        # maximization updating the mean per each cluster
-        m_num = np.sum((g[i, :, np.newaxis] * X), axis=0)
-        m_den = np.sum(g[i], axis=0)
-        m[i] = m_num / m_den
-        # maxization updating of the covarianze matrixes per cluster
-        s_num = np.dot(g[i] * (X - m[i]).T, (X - m[i]))
-        S[i] = s_num / np.sum(g[i])
-        # maximization updating of the prior per each cluster, k
-        pi[i] = np.sum(g[i]) / n
+        for i in range(k):
+            # maximization updating the mean per each cluster
+            m_num = np.sum((g[i, :, np.newaxis] * X), axis=0)
+            m_den = np.sum(g[i], axis=0)
+            m[i] = m_num / m_den
+            # maxization updating of the covarianze matrixes per cluster
+            s_num = np.dot(g[i] * (X - m[i]).T, (X - m[i]))
+            S[i] = s_num / np.sum(g[i])
+            # maximization updating of the prior per each cluster, k
+            pi[i] = np.sum(g[i]) / n
 
-    return (pi, m, S)
+        return (pi, m, S)
+
+    except Exception:
+        return None, None, None
