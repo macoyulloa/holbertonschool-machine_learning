9c97a0997027ca328ccac310f15ab8c31db53f6b
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-06-02 19:56:17 +0000

    load all main files, the README file and the initializing of the variables from the Gaussian Process

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"Class GaussianProces: represents a noiseless 1D Gaussian process"
+
+import numpy as np
+
+
+class GaussianProcess():
+    "represents a noiseless 1D Gaussian process"
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """ Initialized the variables
+
+        Arg:
+        - X_init: np.ndarray of shape (t, 1) representing the inputs
+                    already sampled with the black-box function
+        - Y_init: np.ndarray of shape (t, 1) representing the outputs
+                    of the black-box function for each input in X_init
+        - t: is the number of initial samples
+        - l: is the length parameter for the kernel
+        - sigma_f: is the standard deviation given to the output of the
+                    black-box function
+        Public instance attributes:
+        - X, Y, l, and sigma_f : with the respective constructor inputs
+        - K, representing the current covariance kernel matrix for the
+            Gaussian process
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """ calculates the covariance kernel matrix between two matrices
+
+        Arg:
+            - X1 is a numpy.ndarray of shape (m, 1)
+            - X2 is a numpy.ndarray of shape (n, 1)
+
+        Returns: covariance kernel matrix as a np.ndarray of shape (m, n)
+        """
+        sqdist = (np.sum(X1**2, 1).reshape(-1, 1)) + \
+            (np.sum(X2**2, 1)) - (2 * np.dot(X1, X2.T))
+        return np.exp(-1/2 * sqdist)
+
+    def predict(self, X_s):
+        """ predicts the mean and standard desv of a points in GP
+
+        Arg:
+        - X_s: is a numpy.ndarray of shape (s, 1) with all of the points
+            whose mean and standard deviation should be calculated
+            - s: is the number of sample points
+
+        Returns: mu, sigma
+            - mu: np.ndarray of shape (s,) containing the mean for
+                    each point in X_s, respectively
+            - sigma: np.ndarray of shape (s,) with the standard
+                    desviation for each point in X_s, respectively
+        """
