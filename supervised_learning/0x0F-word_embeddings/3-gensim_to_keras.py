99fe568c47a3ae4488ea13e8ad3ca1c41dc0cf1f
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-07-17 21:42:19 +0000

    covert the gensim layer to keras layer

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Word embedding models"""
 
-import gensim
-import tensorflow as tf
-
 
 def gensim_to_keras(model):
     """converts the gensim word2vec model to a keras layer:
+
+    Requirements:
+        - keras version 2.2.2 needs to be install to succes,
+          with tesorflow as backend.
+              pip3 install heras=2.2.2
 
     Arg:
         - model: is a trained gensim word2vec models
 
     Returns: the trainable keras Embedding
     """
-    print(model)
-    return tf.keras.layers.Embedding(input_dim=model.corpus_count,
-                                     output_dim=model.size)
+    return model.wv.get_keras_embedding(train_embeddings=False)
