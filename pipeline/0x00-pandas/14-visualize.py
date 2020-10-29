ad57fdfca57a5f8c5253f7cedb0afcf282201b0a
Maria Alejandra Coy <764@holbertonschool.com>
2020-10-29 15:18:45 -0500

    viasualized

--- pipeline/0x00-pandas/14-visualize.py
+++ pipeline/0x00-pandas/14-visualize.py
@@ -8,6 +8,7 @@ from_file = __import__('2-from_file').from_file
 
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = df.loc[df["Timestamp"] >= 1483228800]
 df.drop('Weighted_Price', axis=1, inplace=True)
 df.rename(columns={'Timestamp': 'Date'}, inplace=True)
 df['Date'] = pd.to_datetime(df['Date'], unit='s')
@@ -19,4 +20,5 @@ df["Open"].fillna(value=df.Close.shift(1, axis=0), inplace=True)
 df["Volume_(BTC)"].fillna(0, inplace=True)
 df["Volume_(Currency)"].fillna(0, inplace=True)
 
-print(df.columns.values)
+df.plot()
+plt.show()
