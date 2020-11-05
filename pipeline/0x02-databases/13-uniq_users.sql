d5b7ebb287abeff93b8ccb49433c5327c696695a
Maria Alejandra Coy <malejacoy@hotmail.com>
2020-11-05 02:26:43 +0000

    more queries

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,6 @@
+-- creates table force_name
+-- MySQL server
+CREATE TABLE IF NOT EXISTS users(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255));
